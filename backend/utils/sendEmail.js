30d30b538780d9f1ea8d49440e7dc4d3dd5e7510
PAKALA VAMSHI <vmcskillbridge@gmail.com>
2026-05-20 12:10:30 +0530

    Use verified domain for Resend emails

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,30 +1,19 @@
 const { Resend } = require("resend");
 
-const resend = new Resend(
-  process.env.RESEND_API_KEY
-);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-const sendEmail = async ({
-  to,
-  subject,
-  html,
-}) => {
+const sendEmail = async ({ to, subject, html }) => {
   try {
-    const data =
-      await resend.emails.send({
-        from:
-          "VMC SkillBridge <onboarding@resend.dev>",
-        to,
-        subject,
-        html,
-      });
+    const data = await resend.emails.send({
+      from: "VMC SkillBridge <noreply@vmcskillbridge.com>",
+      to,
+      subject,
+      html,
+    });
 
     console.log("EMAIL SENT:", data);
   } catch (error) {
-    console.log(
-      "EMAIL ERROR:",
-      error
-    );
+    console.log("EMAIL ERROR:", error);
   }
 };
 
