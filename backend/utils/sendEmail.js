a86cb0608e1fffc41cffdb18526bf2439348e93d
PAKALA VAMSHI <vmcskillbridge@gmail.com>
2026-05-20 09:45:37 +0530

    Added email debug logs

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,21 +1,27 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async ({ to, subject, html }) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
 
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
-  await transporter.sendMail({
-    from: `"VMC SkillBridge" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-  });
+    const info = await transporter.sendMail({
+      from: `"VMC SkillBridge" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html,
+    });
+
+    console.log("EMAIL SENT:", info.response);
+  } catch (error) {
+    console.log("EMAIL ERROR:", error);
+  }
 };
 
 module.exports = sendEmail;
