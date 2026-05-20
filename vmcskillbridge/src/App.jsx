76ed9d6213cdf181cf0b95e16b161bc50d8fe457
PAKALA VAMSHI <vmcskillbridge@gmail.com>
2026-05-20 13:12:28 +0530

    Hide navbar and footer on admin pages

--- vmcskillbridge/src/App.jsx
+++ vmcskillbridge/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -7,28 +7,43 @@ import Home from "./pages/Home";
 import Careers from "./pages/Careers";
 import Apply from "./pages/Apply";
 import Contact from "./pages/Contact";
+
 import AdminLogin from "./admin/AdminLogin";
 import AdminDashboard from "./admin/AdminDashboard";
+
+function Layout() {
+  const location = useLocation();
+
+  const isAdminPage =
+    location.pathname.startsWith("/admin");
+
+  return (
+    <>
+      {!isAdminPage && <Navbar />}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/careers" element={<Careers />} />
+        <Route path="/apply" element={<Apply />} />
+        <Route path="/contact" element={<Contact />} />
+
+        <Route path="/admin" element={<AdminLogin />} />
+        <Route
+          path="/admin/dashboard"
+          element={<AdminDashboard />}
+        />
+      </Routes>
+
+      {!isAdminPage && <Footer />}
+    </>
+  );
+}
 
 function App() {
   return (
     <BrowserRouter>
       <div className="app">
-        <Navbar />
-
-        <Routes>
-  <Route path="/" element={<Home />} />
-  <Route path="/careers" element={<Careers />} />
-  <Route path="/apply" element={<Apply />} />
-  <Route path="/contact" element={<Contact />} />
-
-  <Route path="/admin" element={<AdminLogin />} />
-  <Route
-    path="/admin/dashboard"
-    element={<AdminDashboard />}
-  />
-</Routes>
-        <Footer />
+        <Layout />
       </div>
     </BrowserRouter>
   );
